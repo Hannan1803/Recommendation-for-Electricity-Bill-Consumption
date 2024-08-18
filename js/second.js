437ed7608f1023fc78a37426099a69552538d116
Hannan1803 <mdhannan1827@gmail.com>
2024-08-18 12:45:55 +0530

    Updated now

--- js/second.js
+++ js/second.js
@@ -11,20 +11,24 @@ document.addEventListener('DOMContentLoaded', () => {
         if (event.target.type === 'checkbox') {
             // If the checkbox is clicked, do not affect the glow directly
             if (checkbox.checked) {
-                clickedElement.style.boxShadow = '0px 4px 8px rgba(5, 5, 5, 0.5)';
+                //clickedElement.style.boxShadow = '0px 4px 8px rgba(5, 5, 5, 0.5)';
+                clickedElement.classList.add('glow');
             } else {
                 // If checkbox is unchecked, remove the glow
-                clickedElement.style.boxShadow = '';
+                //clickedElement.style.boxShadow = '';
+                //clickedElement.classList.remove('glow');
             }
         } else {
             // If the image or description is clicked
-            if (clickedElement.style.boxShadow) {
+            if (clickedElement.classList.contains('glow')) {
                 // If glow is already on, remove the glow and uncheck the checkbox
-                clickedElement.style.boxShadow = '';
+                //clickedElement.style.boxShadow = '';
+                clickedElement.classList.remove('glow');
                 if (checkbox) checkbox.checked = false;
             } else {
                 // Apply the glow
-                clickedElement.style.boxShadow = '0px 4px 8px rgba(5, 5, 5, 0.5)';
+                //clickedElement.style.boxShadow = '0px 4px 8px rgba(5, 5, 5, 0.5)';
+                clickedElement.classList.add('glow');
             }
         }
         console.log("CHECKED ID:", checkbox.checked);
