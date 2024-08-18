95f071cd85e91596c7b84041950fab0dabd4f078
Hannan1803 <mdhannan1827@gmail.com>
2024-08-18 18:44:57 +0530

    Updated js

--- js/second.js
+++ js/second.js
@@ -1,21 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
     const container = document.querySelector('.mid');
-
     container.addEventListener('click', function(event) {
         const clickedElement = event.target.closest('.images');
-        
+    
         if (!clickedElement) return; // Ignore clicks outside .images elements
         
         const checkbox = clickedElement.querySelector('input[type="checkbox"]');
+        const qtyInput = clickedElement.querySelector('input[type="number"]');
 
+        if (event.target === qtyInput) {
+            // If the quantity input box is clicked
+            clickedElement.classList.add('glow');
+            qty.classList.add('glow');
+            console.log("authiten")
+            return
+        }
         if (event.target.type === 'checkbox') {
             // If the checkbox is clicked, do not affect the glow directly
             if (checkbox.checked) {
-                //clickedElement.style.boxShadow = '0px 4px 8px rgba(5, 5, 5, 0.5)';
                 clickedElement.classList.add('glow');
             } else {
-                // If checkbox is unchecked, remove the glow
-                //clickedElement.style.boxShadow = '';
                 //clickedElement.classList.remove('glow');
             }
         } else {
@@ -26,7 +30,6 @@ document.addEventListener('DOMContentLoaded', () => {
                 clickedElement.classList.remove('glow');
                 if (checkbox) checkbox.checked = false;
             } else {
-                // Apply the glow
                 //clickedElement.style.boxShadow = '0px 4px 8px rgba(5, 5, 5, 0.5)';
                 clickedElement.classList.add('glow');
             }
@@ -34,4 +37,5 @@ document.addEventListener('DOMContentLoaded', () => {
         console.log("CHECKED ID:", checkbox.checked);
         console.log("Clicked ID:", clickedElement.id);
     });
+
 });
